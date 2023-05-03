8cc4b09bec5781accfddb90ce8f4352e2633e9cb
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2023-05-03 15:05:15 +0200

    chef data has been provided to the front for fetching and fetch completed

--- index.js
+++ index.js
@@ -4,10 +4,66 @@ const port = 5000;
 const cors = require('cors');
 app.use(cors());
 
+const chefs = require('./data/chefs.json');
+
+// app.get('/', (req, res) => {
+//   res.send('Server is running successfully');
+// });
+
 app.get('/', (req, res) => {
-  res.send('Server is running successfully');
+  res.send(chefs);
 });
 
 app.listen(port, () => {
   console.log('Server is running successfully');
 });
+
+/* 
+
+// import express from 'express';
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+
+const categories = require('./data/categories.json');
+const allNews = require('./data/news.json');
+
+app.get('/', (req, res) => {
+  res.send('testing');
+});
+
+app.get('/categories', (req, res) => {
+  res.send(categories);
+});
+
+app.get('/categories/:id', (req, res) => {
+  const id = req.params.id;
+  if (parseFloat(id) === 0) {
+    res.send(allNews);
+  } else {
+    const filtered = allNews.filter((news) => news.category_id === id);
+    res.send(filtered);
+  }
+});
+
+app.get('/allnews', (req, res) => {
+  res.send(allNews);
+});
+
+app.get('/allnews/:id', (req, res) => {
+  const id = req.params.id;
+  const found = allNews.find((news) => news._id === id);
+  res(found);
+});
+
+app.listen(port, () => {
+  console.log(`Good`);
+});
+
+
+
+
+*/
